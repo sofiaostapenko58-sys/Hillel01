5251d24f31f9d6addb2a20eea91b49a90ee7206d
sofia <sofiaostapenko58@gmail.com>
2026-09-01 21:06:54 +0200

    Homework 5.2

---
+++ 02.05.py
@@ -0,0 +1,20 @@
+while True :
+
+   first_number = int(input("Enter a number: "))
+   second_number = int(input("Enter another number: "))
+
+   action = input("Enter(+,*,-,/) :")
+   if action == "+":
+     print(first_number + second_number)
+   elif action == "-":
+        print(first_number - second_number)
+   elif action == "*":
+       print(first_number * second_number)
+   elif action == "/":
+       if second_number != 0:
+         print(first_number / second_number)
+       else:
+         print('Second number cannot be zero')
+   again = input("Do you want to continue?: ")
+   if again != "yes" :
+      break
